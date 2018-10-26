2f775c306926582e40716c90c8bc0587abe06368
Marie Andrée Roy <maroy@outbox.com>
2018-10-26 00:03:21 -0400

    Intro

---
+++ RedBird.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+worlds = {
+    '1': "Rain forest hipster",
+    '2': "Steampunk underground township",
+    '3': "Radiated misty city",
+    '4': "Techno elitist planet",
+    '5': "Blackout tunnel",
+}
+
+
+def print_worlds():
+
+    for key, value in worlds.items():
+        print(f"{key}: {value}")
+
+
+def welcome_user():
+    username = input("Please, enter your character's nickname for the game: \n")
+
+    if not username:
+        username = "Pepito"
+
+    print(f"Welcome, {username}, to this very unique game called Red bird!")
+
+    return username
+
+
+def select_world(username):
+    print(f"Please, select a world to explore: ")
+    print_worlds()
+    world = input()
+    print(f"You chose wisely, {username}. Be careful and look out for the red birds in {worlds[world]}.")
+
+
+class WelcomeToRedBirdGame:
+    username = welcome_user()
+    select_world(username)
