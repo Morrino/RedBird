a00ab37c91efec6626a6121f6f299a23bf246d8e
François Drolet <fdrolet@outbox.com>
2018-10-28 23:43:54 -0400

    extract world debug

--- worlds/world.py
+++ worlds/world.py
@@ -15,18 +15,23 @@ class RandomWorld:
   x is horizontal (index of width)
   y is vertical   (index of height)
   """
+
+  def _debug_world_1(selfie):
+    for y in range(selfie.height):
+      for x in range(selfie.width):
+        print(f'{x},{y} = {selfie.world[y][x].description}')
+
   def __init__(selfie, description='a randomly generated world', w=5, h=8):
     selfie.description = description
+    selfie.width = w
+    selfie.height = h
     selfie.density = 0.5
     selfie.freedom = 0.8
 
-    world = [[Room(f'This room is at {x},{y}') for x in range(w)] for y in range(h)]
+    selfie.world = [[Room(f'This room is at {x},{y}') for x in range(w)] for y in range(h)]
 
-    # print world
-    for y in range(h):
-      for x in range(w):
-        print(f'{x},{y} = {world[y][x].description}')
+    #selfie._debug_world_1()
 
     # get a list of existing rooms and choice one
     # flatten world and choice
-    selfie.start = world[0][0]
+    selfie.start = selfie.world[0][0]
